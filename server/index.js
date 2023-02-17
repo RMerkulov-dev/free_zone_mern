a584722549185eda64d23b40d36ef29e6ef5580d
RMerkulov <fotoromario@gmail.com>
2023-02-17 16:56:44 +0200

    upd

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
-import { verifyToken } from "./middleware/auth.js";
 
 //CONFIGURATIONS
 
@@ -47,6 +47,7 @@ app.post("/auth/register", upload.single("picture"), register);
 //ROTES
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 //MONGOOSE SETUP
 
