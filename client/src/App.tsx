4bc6d3b1551488f9a937ef573b35bf4428e106ce
RMerkulov <fotoromario@gmail.com>
2023-02-20 18:15:27 +0200

    upd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,25 @@ import LoginPage from "./views/loginPage";
 import ProfilePage from "./views/profilePage";
 
 import { Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+import { useAppSelector } from "./hooks";
+import { useMemo } from "react";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
 export const App = () => {
+  const mode = useAppSelector((state) => state.mode);
+  // @ts-ignore
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 };
