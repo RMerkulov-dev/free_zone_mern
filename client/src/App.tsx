5c71dd2b78bddbc8ff916c1a9cc1e7710f612a0a
RMerkulov <fotoromario@gmail.com>
2023-02-20 10:12:23 +0200

    add_router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
+import HomePage from "./views/homePage";
+import LoginPage from "./views/loginPage";
+import ProfilePage from "./views/profilePage";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div className="App">
-      <h1>MERN APP</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
+
+//TODO:
+//Video 01:55:23
