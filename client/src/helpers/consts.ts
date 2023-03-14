ceed687d77fbac3224bdd51c4461690a55c50953
RMerkulov <fotoromario@gmail.com>
2023-03-14 22:03:42 +0200

    upd

--- client/src/helpers/consts.ts
+++ client/src/helpers/consts.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL = "https://freezone-y2t5.onrender.com";
 // export const BASE_URL = "https://free-zone-mern-ke4e.vercel.app/";
-export const BASE_URL = "freezonemern-production.up.railway.app";
+export const BASE_URL = "https://freezonemern-production.up.railway.app/";
 
 // export const BASE_URL = "http://localhost:3001";
