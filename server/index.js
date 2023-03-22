35e5c62b420aa983e9922b9e9cfbdf96999d07fb
RMerkulov <fotoromario@gmail.com>
2023-03-22 12:27:04 +0200

    upd assets

--- server/index.js
+++ server/index.js
@@ -31,7 +31,10 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "./public/assets")));
+app.use(
+  "/public/assets",
+  express.static(path.join(__dirname, "./public/assets"))
+);
 mongoose.set("strictQuery", false);
 
 //FILE STORAGE
