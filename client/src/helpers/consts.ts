bedbb19d61375ef61903bc336c7829a9e0ec8719
RMerkulov <fotoromario@gmail.com>
2023-03-22 13:53:26 +0200

    upd

--- client/src/helpers/consts.ts
+++ client/src/helpers/consts.ts
@@ -2,4 +2,5 @@
 export const BASE_URL = "https://freezonemern-production.up.railway.app";
 //LOCAL
 // export const BASE_URL = "http://localhost:3001";
+//test
 export const SHARED_URL = "https://free-zone-mern.vercel.app/home";
