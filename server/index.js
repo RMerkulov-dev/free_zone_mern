ad2b41b86462f50cd9d06915e036787da114725b
RMerkulov <fotoromario@gmail.com>
2023-03-22 14:04:04 +0200

    upd

--- server/index.js
+++ server/index.js
@@ -31,33 +31,33 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "./public/assets")));
+app.use("/assets", express.static(path.join(__dirname, "/public/assets")));
 mongoose.set("strictQuery", false);
 
 //FILE STORAGE
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
-    fileFilter: function (req, file, cb) {
-        const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
-        if (!allowedTypes.includes(file.mimetype)) {
-            const error = new Error("Wrong file type");
-            error.code = "LIMIT_FILE_TYPES";
-            return cb(error, false);
-        }
-        if (file.size > 5 * 5024 * 5024) {
-            // 5MB limit
-            const error = new Error("File too large");
-            error.code = "LIMIT_FILE_SIZE";
-            return cb(error, false);
-        }
-        cb(null, true);
-    },
+  destination: function (req, file, cb) {
+    cb(null, "/public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+  fileFilter: function (req, file, cb) {
+    const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+    if (!allowedTypes.includes(file.mimetype)) {
+      const error = new Error("Wrong file type");
+      error.code = "LIMIT_FILE_TYPES";
+      return cb(error, false);
+    }
+    if (file.size > 5 * 5024 * 5024) {
+      // 5MB limit
+      const error = new Error("File too large");
+      error.code = "LIMIT_FILE_SIZE";
+      return cb(error, false);
+    }
+    cb(null, true);
+  },
 });
 
 const upload = multer({ storage });
@@ -65,28 +65,28 @@ const upload = multer({ storage });
 //ROUTES WITH FILES
 
 app.post(
-    "/auth/register",
-    upload.single("picture"),
-    register,
-    register,
-    (err, req, res, next) => {
-        if (err instanceof multer.MulterError && err.code === "LIMIT_FILE_TYPES") {
-            return res.status(400).json({ error: "Wrong file type" });
-        }
-        next(err);
+  "/auth/register",
+  upload.single("picture"),
+  register,
+  register,
+  (err, req, res, next) => {
+    if (err instanceof multer.MulterError && err.code === "LIMIT_FILE_TYPES") {
+      return res.status(400).json({ error: "Wrong file type" });
     }
+    next(err);
+  }
 );
 app.post(
-    "/posts",
-    verifyToken,
-    upload.single("picture"),
-    createPost,
-    (err, req, res, next) => {
-        if (err instanceof multer.MulterError && err.code === "LIMIT_FILE_TYPES") {
-            return res.status(400).json({ error: "Wrong file type" });
-        }
-        next(err);
+  "/posts",
+  verifyToken,
+  upload.single("picture"),
+  createPost,
+  (err, req, res, next) => {
+    if (err instanceof multer.MulterError && err.code === "LIMIT_FILE_TYPES") {
+      return res.status(400).json({ error: "Wrong file type" });
     }
+    next(err);
+  }
 );
 
 //ROTES
@@ -97,15 +97,15 @@ app.use("/posts", postsRoutes);
 
 const PORT = process.env.PORT || 6001;
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server Ok Port: ${PORT}`));
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Ok Port: ${PORT}`));
 
-        //ADD DATA ONE TIME/ NEED TO DELETE
-        // User.insertMany(users);
-        // Post.insertMany(posts);
-    })
-    .catch((error) => console.log(`${error} did not connect`));
+    //ADD DATA ONE TIME/ NEED TO DELETE
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
