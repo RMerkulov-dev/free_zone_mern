2bba4fb21ee57e195a5e283b4f8d98dd649d22b1
RMerkulov <fotoromario@gmail.com>
2023-03-22 16:26:52 +0200

    upd

--- server/index.js
+++ server/index.js
@@ -38,13 +38,13 @@ mongoose.set("strictQuery", false);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    cb(null, "./public/assets");
+  },
+  filename: function (req, file, cb) {
     cb(
       null,
       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
     );
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
   },
   fileFilter: function (req, file, cb) {
     const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
