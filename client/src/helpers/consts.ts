52bef8e172751ed7b626142a86b0a5aeae1167ba
RMerkulov <fotoromario@gmail.com>
2023-05-05 13:05:30 +0300

    upd

--- client/src/helpers/consts.ts
+++ client/src/helpers/consts.ts
@@ -1,5 +1,5 @@
 //RAILWAY
-export const BASE_URL = "https://free-zone-server.vercel.app/";
+export const BASE_URL = "https://free-zone-server.vercel.app";
 //LOCAL
 // export const BASE_URL = "http://localhost:3001";
 
